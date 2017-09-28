58bb7387dd4f95f891ebb8e314f8ba951248f463
lloydering97 <lloydering97@gmail.com>
2017-09-27 18:08:05 -0700

    Implemented multiple simultaneous clients.

--- Assignment 1/EchoServer.java
+++ Assignment 1/EchoServer.java
@@ -10,28 +10,37 @@ import java.io.InputStreamReader;
 
 public final class EchoServer {
 
+    private static Socket clientSocket;
+
     public static void main(String[] args) throws Exception {
         try (ServerSocket serverSocket = new ServerSocket(22222)) {
-            while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    String address = socket.getInetAddress().getHostAddress();
-                    InputStream is = socket.getInputStream();
-                    InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-                    BufferedReader br = new BufferedReader(isr);
-                    System.out.printf("Client connected: %s%n", address);
-                    OutputStream os = socket.getOutputStream();
-                    PrintStream out = new PrintStream(os, true, "UTF-8");
-                    out.printf("Hi %s, thanks for connecting!%n", address);
-                    while( true ){
-                        String input = br.readLine();
-                        if( input == null ){
-                            System.out.printf("Client disconnected: %s%n", address);
-                            break;
+            while (true) { 
+                    Runnable session = () -> {   
+                        try{  
+                            Socket socket = clientSocket;
+                            String address = socket.getInetAddress().getHostAddress();
+                            InputStream is = socket.getInputStream();
+                            InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+                            BufferedReader br = new BufferedReader(isr);
+                            System.out.printf("Client connected: %s%n", address);
+                            OutputStream os = socket.getOutputStream();
+                            PrintStream out = new PrintStream(os, true, "UTF-8");
+                            out.printf("Hi %s, thanks for connecting!%n", address);
+                            while( true ){
+                                String input = br.readLine();
+                                if( input == null ){
+                                    System.out.printf("Client disconnected: %s%n", address);
+                                    break;
+                                }
+                                out.println( input );
+                            }
                         }
-                        out.println( input );
-                    }
-                    break;
-                }
+                        catch(Exception e)
+                        {}
+                    };
+                    clientSocket = serverSocket.accept();
+                    Thread newSession = new Thread(session);
+                    newSession.start();
             }
         }
     }
